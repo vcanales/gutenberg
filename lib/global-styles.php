eadcc6331deccd3b985badb0aa5b40e1b18dda04
Andrés <andres.maneiro@automattic.com>
2020-06-22 18:42:40 +0200

    Show user data in controls

--- lib/global-styles.php
+++ lib/global-styles.php
@@ -97,11 +97,18 @@ function gutenberg_experimental_global_styles_get_from_file( $file_path ) {
 /**
  * Returns the user's origin config.
  *
+ * @param boolean $include_draft Whether to include draft or only publish CPTs.
+ *
  * @return array Config that adheres to the theme.json schema.
  */
-function gutenberg_experimental_global_styles_get_user() {
-	$config   = array();
-	$user_cpt = gutenberg_experimental_global_styles_get_user_cpt( array( 'publish' ) );
+function gutenberg_experimental_global_styles_get_user( $include_draft = false ) {
+	$config             = array();
+	$post_status_filter = array( 'publish' );
+	if ( $include_draft ) {
+		$post_status_filter[] = 'draft';
+	}
+
+	$user_cpt = gutenberg_experimental_global_styles_get_user_cpt( $post_status_filter );
 	if ( array_key_exists( 'post_content', $user_cpt ) ) {
 		$decoded_data = json_decode( $user_cpt['post_content'], true );
 
@@ -538,13 +545,15 @@ function gutenberg_experimental_global_styles_normalize_schema( $tree ) {
  * Returns the stylesheet resulting of merging
  * core's, theme's, and user's origins.
  *
+ * @param boolean $include_draft Whether to include draft or only publish CPT for user styles.
+ *
  * @return string
  */
-function gutenberg_experimental_global_styles_get_stylesheet() {
+function gutenberg_experimental_global_styles_get_stylesheet( $include_draft = false ) {
 	$gs_merged = array();
 	$gs_core   = gutenberg_experimental_global_styles_get_core();
 	$gs_theme  = gutenberg_experimental_global_styles_get_theme();
-	$gs_user   = gutenberg_experimental_global_styles_get_user();
+	$gs_user   = gutenberg_experimental_global_styles_get_user( $include_draft );
 
 	$gs_merged = gutenberg_experimental_global_styles_merge_trees( $gs_core, $gs_theme, $gs_user );
 
@@ -576,21 +585,16 @@ function gutenberg_experimental_global_styles_enqueue_assets() {
  */
 function gutenberg_experimental_global_styles_settings( $settings ) {
 
+	$include_draft = true;
 	if ( gutenberg_experimental_global_styles_has_theme_json_support() ) {
+		$settings['__experimentalGlobalStylesUser']         = gutenberg_experimental_global_styles_get_user( $include_draft );
 		$settings['__experimentalGlobalStylesUserEntityId'] = gutenberg_experimental_global_styles_get_user_cpt_id();
-
-		$global_styles = gutenberg_experimental_global_styles_merge_trees(
-			gutenberg_experimental_global_styles_get_core(),
-			gutenberg_experimental_global_styles_get_theme()
-		);
-
-		$settings['__experimentalGlobalStylesBase'] = $global_styles;
 	}
 
 	// Add the styles for the editor via the settings
 	// so they get processed as if they were added via add_editor_styles:
 	// they will get the editor wrapper class.
-	$settings['styles'][] = array( 'css' => gutenberg_experimental_global_styles_get_stylesheet() );
+	$settings['styles'][] = array( 'css' => gutenberg_experimental_global_styles_get_stylesheet( $include_draft ) );
 
 	return $settings;
 }
